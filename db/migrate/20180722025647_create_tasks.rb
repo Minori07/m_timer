c3dc1c9134c9cfa9e636573c7425a71f37cfa94e
高村美紀 <takamuraminori@koumuraikinoMBP.lan>
2018-08-07 14:39:55 +0900

    途中経過

--- db/migrate/20180722025647_create_tasks.rb
+++ db/migrate/20180722025647_create_tasks.rb
@@ -3,17 +3,11 @@ class CreateTasks < ActiveRecord::Migration[5.2]
     create_table :tasks do |t|
     	# タイトル
       t.string :title, null: false
-      # 説明
-      # t.text :description, null: true
       #タスクの曜日
       t.integer :week, null: true
       # 時間
-      # t.datetime :time, null: true
-      t.toDo[] :todo, null: true
-      # タスク完了日
-      # t.datetime :completed_at, null: true
-      # タスク完了したか
-      t.boolean :completed, null: false, default: false
+      t.datetime :time, null: true
+      # t.toDo[] :todo, null: true
       # userモデルのuser_idに結びつけるため、必要
       t.integer :user_id, null: true
       t.timestamps
