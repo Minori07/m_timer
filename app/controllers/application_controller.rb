251cab440f3b8b0a500f750c5f88da6dc12553b9
高村美紀 <takamuraminori@koumuramikinoMacBook-Pro.local>
2018-08-17 07:37:31 +0900

    timezonecharenge

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,6 @@
 class ApplicationController < ActionController::Base
 	before_action :configure_permitted_parameters, if: :devise_controller?
-  
-  around_action :set_time_zone, if: :user_time_zone_present?
- 
-  private
-    def set_time_zone(&block)
-      Time.use_zone(current_user.time_zone, &block)
-    end
- 
-    def user_time_zone_present?
-      current_user.try(:time_zone).present?
-    end
+
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :username])
